bfb8a7700dc26fe5c98776aef0ff8e7bdc6fb86d
tangshangmin <441953232@qq.com>
2020-11-22 22:19:34 +0800

    多线程学习

---
+++ learn/src/main/java/com/example/mutilpulthread/reentrancelock/LockClass1.java
@@ -0,0 +1,37 @@
+package com.example.mutilpulthread.reentrancelock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LockClass1 implements Runnable{
+    private Integer key = 0;
+
+    // 锁对象
+    private Lock lock = new ReentrantLock();
+
+    @Override
+    public void run() {
+        // 需要结果是key实现自增长，如果没有同步块，则可能会出现重复key值的现象
+        for(int j=0;j<100000;j++) {
+        lock.lock();
+            try {
+                key++;
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        LockClass1 lt = new LockClass1();
+
+        Thread thread1 = new Thread(lt);
+        Thread thread2 = new Thread(lt);
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println("key == " + lt.key);
+    }
+
+}
---
+++ learn/src/main/java/com/example/mutilpulthread/synchronize/DisappearRequest1.java
@@ -0,0 +1,42 @@
+package com.example.mutilpulthread.synchronize;
+
+/**
+/**
+ * 不使用并发手段的后果演示
+ * 两个线程同时a++，最后结果会比预计的少
+ * 原因：count++，它看上去只是一个操作，实际上包含了三个动作：
+ *
+ * 1、读取count
+ *
+ * 2、将count加一
+ *
+ * 3、将count的值写入到内存中
+ */
+public class DisappearRequest1 implements Runnable{
+    public static int i = 0;
+
+    @Override
+    public void run() {
+        for(int j=0; j<100000; j++){
+            synchronized (this) {
+                i++;
+            }
+        }
+
+        /*for(int j=0; j<100000; j++){
+                i++;
+        }*/
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        DisappearRequest1 disappearRequest1 = new DisappearRequest1();
+        Thread thread1 = new Thread(disappearRequest1);
+        Thread thread2 = new Thread(disappearRequest1);
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println("i= " + i);
+    }
+}
